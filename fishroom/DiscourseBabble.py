13b61c14fdae73133671652df666de14780a9f70
misaka4e21 <misaka4e21@gmail.com>
2018-08-12 07:41:05 +0800

    Support native Discourse Webhook for new Babble versions

--- fishroom/DiscourseBabble.py
+++ fishroom/DiscourseBabble.py
@@ -45,10 +45,17 @@ def getWebhookHandler(dbh):
     class WebhookHandler(tornado.web.RequestHandler):
         def post(self):
             json = tornado.escape.json_decode(self.request.body)
-            topic_id = json.get('topic_id',0)
-            current_user = json.get('current_user', '未知用户')
-            message = json.get('message', '未知消息')
-            dbh.on_sendmessage(topic_id, current_user, message)
+            post = json.get('post', None)
+            if post:
+                topic_id = post.get('id', 0)
+                current_user = post.get('username', '未知用户')
+                message = post.get('cooked', '未知消息')
+                dbh.on_sendmessage(topic_id, current_user, message)
+            else:
+                topic_id = json.get('topic_id',0)
+                current_user = json.get('current_user', '未知用户')
+                message = json.get('message', '未知消息')
+                dbh.on_sendmessage(topic_id, current_user, message)
             self.write('Got a message.')
     return WebhookHandler
                 
